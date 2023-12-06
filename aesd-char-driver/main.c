fc3213a81b622e921f12a7d7db6efd28c99cd4ff
Danny Deng <zxpay73945788@gmail.com>
2023-12-06 22:03:17 +0800

    temp

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -132,17 +132,18 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     }
 
     struct aesd_buffer_entry entry;
-    char *malloc_buf = kmalloc(count * sizeof(char) + 1, GFP_ATOMIC);
-    if(NULL == malloc_buf) {
-        PDEBUG("can not malloc\n");
-        mutex_unlock(&aesd_lock);
-        return -ENOMEM;
-    }
-    memcpy(malloc_buf, buf, count*sizeof(char)+1);
+    // char *malloc_buf = kmalloc(count * sizeof(char) + 1, GFP_ATOMIC);
+    // if(NULL == malloc_buf) {
+        // PDEBUG("can not malloc\n");
+        // mutex_unlock(&aesd_lock);
+        // return -ENOMEM;
+    // }
+    // memcpy(malloc_buf, buf, count*sizeof(char)+1);
     entry.size = count;
-    entry.buffptr = malloc_buf;
+    // entry.buffptr = malloc_buf;
+    entry.buffptr = buf;
     aesd_circular_buffer_add_entry(&aesd_device.cir_buf_, &entry);
-    printk(KERN_INFO "%s", malloc_buf);
+    // printk(KERN_INFO "%s", malloc_buf);
     mutex_unlock(&aesd_lock);
     if(aesd_device.count_ < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
     {
